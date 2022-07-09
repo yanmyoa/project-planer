ba797a558928b491bbdf6a8915d78e6d0937c392
YanMyoAung <yanmyoaung.mdy1500@gmail.com>
2022-07-09 19:34:05 +0630

    9.7.2022 project planer lesson 3

---
+++ src/components/SingleProject.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="project">
+        <div class="flexing">
+            <div>
+                <h3 @click="showDetail=!showDetail">{{project.title}}</h3>
+            </div>
+            <div>
+                <span class="material-icons">
+                delete
+                </span>
+                <span class="material-icons">
+                edit
+                </span>
+                <span class="material-icons">
+                done
+                </span>
+            </div>
+        </div>
+        <p v-if="showDetail">{{project.detail}}</p>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    props:['project'],
+    data(){
+        return{
+            showDetail:false
+        }
+    }
+}
+</script>
+
+<style>
+    .project{
+        padding:20px;
+        background-color: #f2f2f2;
+        margin: 10px;
+        border-left: 6px solid crimson;
+        border-radius: 15px
+    }
+
+    h3{
+        cursor: pointer;
+        color: indigo;
+    }
+    .flexing{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+    span{
+        margin-left: 7px;
+    }
+    span:hover{
+        cursor: pointer;
+        color: #777;
+    }
+</style>
